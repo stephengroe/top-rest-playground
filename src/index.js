a61e438cc8f2c08ca9caf26c38c14f3e356f73f8
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-12-19 12:49:23 -0700

    Add data GET routes
    Add sample data file, which is imported into index.js and performs basic GET functions, mimicking DB operations.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const users = require('./sample-data').users;
+const messages = require('./sample-data').messages;
 
 // Run express
 const app = express();
 app.use(cors());
 
-// Basic routes
+//// HOME ROUTES ////
 app.get('/', (req, res) => {
   res.send('Welcome to the home page!');
 });
 
+//// USER ROUTES ////
 app.get('/users', (req, res) => {
-  return res.send('Received a GET HTTP method on user resource!');
+  return res.send(Object.values(users));
+});
+
+app.get('/users/:id', (req, res) => {
+  return res.send(users[req.params.id]);
 });
 
 app.post('/users', (req, res) => {
@@ -30,6 +37,15 @@ app.delete('/users/:id', (req, res) => {
   return res.send(
     `Received a DELETE HTTP method on user resource! ID: ${req.params.id}`
     );
+});
+
+//// MESSAGE ROUTES ////
+app.get('/messages', (req, res) => {
+  res.send(Object.entries(messages));
+});
+
+app.get('/messages/:id', (req, res) => {
+  res.send(messages[req.params.id]);
 });
 
 // Set up app to listen
---
+++ src/sample-data.js
@@ -0,0 +1,26 @@
+let users = {
+  1: {
+    id: '1',
+    username: 'Alice',
+  },
+  2: {
+    id: '2',
+    username: 'Bob',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello, World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Bye, World',
+    userId: '2',
+  },
+};
+
+module.exports.users = users;
+module.exports.messages = messages;
