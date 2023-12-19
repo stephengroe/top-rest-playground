3f55259a92ffd1833ab59ae97e8bd8424d27e75b
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-12-19 12:33:39 -0700

    Add HTTP method routes
    Includes routes for GET, POST, PUT, and DELETE for users, including ID parameter.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 // Imports
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 // Run express
 const app = express();
+app.use(cors());
 
 // Basic routes
-app.get('/', (req, res, next) => {
-  res.send('This is the home page');
+app.get('/', (req, res) => {
+  res.send('Welcome to the home page!');
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+app.get('/users', (req, res) => {
+  return res.send('Received a GET HTTP method on user resource!');
+});
+
+app.post('/users', (req, res) => {
+  return res.send('Received a POST HTTP method on user resource!');
+});
+
+app.put('/users/:id', (req, res) => {
+  return res.send(
+    `Received a PUT HTTP method on user resource! ID: ${req.params.id}`
+    );
+});
+
+app.delete('/users/:id', (req, res) => {
+  return res.send(
+    `Received a DELETE HTTP method on user resource! ID: ${req.params.id}`
+    );
+});
+
+// Set up app to listen
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}!`);
 });
